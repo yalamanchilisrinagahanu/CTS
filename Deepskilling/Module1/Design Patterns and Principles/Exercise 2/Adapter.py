45a6295fc6dd617dde288f2406318f9278c1df0e
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-06-30 11:02:30 +0530

    Update Adapter.py

--- Deepskilling/Module1/Design Patterns and Principles/Exercise 2/Adapter.py
+++ Deepskilling/Module1/Design Patterns and Principles/Exercise 2/Adapter.py
@@ -1,1 +1,16 @@
+class OldPrinter:
 
+    def old_print(self):
+        return "Old Printer"
+
+class Adapter:
+
+    def __init__(self, old):
+        self.old = old
+
+    def print(self):
+        print(self.old.old_print())
+
+obj = Adapter(OldPrinter())
+
+obj.print()
