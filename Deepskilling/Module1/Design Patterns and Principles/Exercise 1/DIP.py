18b7bc8217425123bd5552f64862be1217302ac0
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-06-30 09:25:44 +0530

    Implement Dependency Inversion Principle with Keyboard class

--- Deepskilling/Module1/Design Patterns and Principles/Exercise 1/DIP.py
+++ Deepskilling/Module1/Design Patterns and Principles/Exercise 1/DIP.py
@@ -1,1 +1,17 @@
+class Keyboard:
 
+    def type(self):
+        print("Typing")
+
+class Computer:
+
+    def __init__(self, keyboard):
+        self.keyboard = keyboard
+
+    def start(self):
+        self.keyboard.type()
+
+keyboard = Keyboard()
+computer = Computer(keyboard)
+
+computer.start()
