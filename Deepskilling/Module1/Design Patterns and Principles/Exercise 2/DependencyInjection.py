c7123fec0dc4f90e89985e40c2a19ff3881ff3de
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-06-30 11:09:21 +0530

    Update DependencyInjection.py

--- Deepskilling/Module1/Design Patterns and Principles/Exercise 2/DependencyInjection.py
+++ Deepskilling/Module1/Design Patterns and Principles/Exercise 2/DependencyInjection.py
@@ -1,1 +1,18 @@
+class Service:
 
+    def serve(self):
+        print("Service Running")
+
+class Client:
+
+    def __init__(self, service):
+        self.service = service
+
+    def execute(self):
+        self.service.serve()
+
+service = Service()
+
+client = Client(service)
+
+client.execute()
