9cfdba26894d841f3735fbeac2880ba627433ee2
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-06-30 09:23:28 +0530

    Update LSP.py

--- Deepskilling/Module1/Design Patterns and Principles/Exercise 1/LSP.py
+++ Deepskilling/Module1/Design Patterns and Principles/Exercise 1/LSP.py
@@ -1,1 +1,16 @@
+class Bird:
+    def move(self):
+        print("Bird is moving")
 
+class Sparrow(Bird):
+    def move(self):
+        print("Sparrow flies")
+
+class Penguin(Bird):
+    def move(self):
+        print("Penguin walks")
+
+birds = [Sparrow(), Penguin()]
+
+for b in birds:
+    b.move()
