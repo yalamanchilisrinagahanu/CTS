e8a956cd438c33146d4adbed2fcae2086658e0dd
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-06-30 11:18:55 +0530

    Implement Single Responsibility Principle with Report classes

--- Deepskilling/Module1/Design Patterns and Principles/Exercise 3/Single Responsibility Principle.cs
+++ Deepskilling/Module1/Design Patterns and Principles/Exercise 3/Single Responsibility Principle.cs
@@ -1,1 +1,31 @@
+using System;
 
+class Report
+{
+    public string Content;
+
+    public Report(string content)
+    {
+        Content = content;
+    }
+}
+
+class ReportPrinter
+{
+    public void PrintReport(Report report)
+    {
+        Console.WriteLine(report.Content);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Report report = new Report("Monthly Report");
+
+        ReportPrinter printer = new ReportPrinter();
+
+        printer.PrintReport(report);
+    }
+}
