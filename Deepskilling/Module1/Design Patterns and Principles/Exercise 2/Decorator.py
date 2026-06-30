6750429fea93d6df0c6c5c7fece027d41f7d6e7a
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-06-30 11:03:21 +0530

    Update Decorator.py

--- Deepskilling/Module1/Design Patterns and Principles/Exercise 2/Decorator.py
+++ Deepskilling/Module1/Design Patterns and Principles/Exercise 2/Decorator.py
@@ -1,1 +1,17 @@
+def decorator(func):
 
+    def wrapper():
+
+        print("Before")
+
+        func()
+
+        print("After")
+
+    return wrapper
+
+@decorator
+def hello():
+    print("Hello")
+
+hello()
