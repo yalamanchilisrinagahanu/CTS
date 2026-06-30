453e22ee8773479cd144403addebf7bbcc3e8097
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-06-30 11:00:26 +0530

    Update Builder.py

--- Deepskilling/Module1/Design Patterns and Principles/Exercise 2/Builder.py
+++ Deepskilling/Module1/Design Patterns and Principles/Exercise 2/Builder.py
@@ -1,1 +1,18 @@
+class House:
 
+    def __init__(self):
+        self.parts = []
+
+    def add(self, part):
+        self.parts.append(part)
+
+    def show(self):
+        print(self.parts)
+
+house = House()
+
+house.add("Door")
+house.add("Window")
+house.add("Roof")
+
+house.show()
