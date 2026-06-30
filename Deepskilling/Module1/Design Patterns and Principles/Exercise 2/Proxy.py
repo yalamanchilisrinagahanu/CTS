17daf89ff0a3ba600cfe8040038676fd9648407e
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-06-30 11:04:13 +0530

    Implement Proxy design pattern for image display

--- Deepskilling/Module1/Design Patterns and Principles/Exercise 2/Proxy.py
+++ Deepskilling/Module1/Design Patterns and Principles/Exercise 2/Proxy.py
@@ -1,1 +1,16 @@
+class RealImage:
 
+    def display(self):
+        print("Displaying Image")
+
+class ProxyImage:
+
+    def __init__(self):
+        self.real = RealImage()
+
+    def display(self):
+        self.real.display()
+
+img = ProxyImage()
+
+img.display()
