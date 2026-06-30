1fdee28efcf15d1f78c5da5262307d7d3d0a1951
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-06-30 11:05:38 +0530

    Update Observer.py

--- Deepskilling/Module1/Design Patterns and Principles/Exercise 2/Observer.py
+++ Deepskilling/Module1/Design Patterns and Principles/Exercise 2/Observer.py
@@ -1,1 +1,24 @@
+class Subject:
 
+    def __init__(self):
+        self.observers = []
+
+    def add(self, obs):
+        self.observers.append(obs)
+
+    def notify(self):
+        for o in self.observers:
+            o.update()
+
+class Observer:
+
+    def update(self):
+        print("Notification Received")
+
+subject = Subject()
+
+observer = Observer()
+
+subject.add(observer)
+
+subject.notify()
