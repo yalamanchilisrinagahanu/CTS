c7a2c29c689c48086dd0ee1907d4ea7249a32d17
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-06-30 11:08:30 +0530

    Update MVC.py

--- Deepskilling/Module1/Design Patterns and Principles/Exercise 2/MVC.py
+++ Deepskilling/Module1/Design Patterns and Principles/Exercise 2/MVC.py
@@ -1,1 +1,21 @@
+class Model:
 
+    def get_data(self):
+        return "Welcome"
+
+class View:
+
+    def display(self, data):
+        print(data)
+
+class Controller:
+
+    def __init__(self):
+        self.model = Model()
+        self.view = View()
+
+    def update(self):
+        data = self.model.get_data()
+        self.view.display(data)
+
+Controller().update()
