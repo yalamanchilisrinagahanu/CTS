d34a899ff0167ede71cd896c80580fa555d970d2
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-06-30 09:48:06 +0530

    Update Factory.py

--- Deepskilling/Module1/Design Patterns and Principles/Exercise 2/Factory.py
+++ Deepskilling/Module1/Design Patterns and Principles/Exercise 2/Factory.py
@@ -1,1 +1,24 @@
+class Car:
 
+    def drive(self):
+        print("Driving Car")
+
+class Bike:
+
+    def drive(self):
+        print("Riding Bike")
+
+class Factory:
+
+    def create(self, vehicle):
+
+        if vehicle == "car":
+            return Car()
+
+        return Bike()
+
+factory = Factory()
+
+obj = factory.create("car")
+
+obj.drive()
