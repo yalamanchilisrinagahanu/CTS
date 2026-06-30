a99bdf1101ad94beb1448e1b63bd6588bfd58e87
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-06-30 09:24:14 +0530

    Update ISP.py

--- Deepskilling/Module1/Design Patterns and Principles/Exercise 1/ISP.py
+++ Deepskilling/Module1/Design Patterns and Principles/Exercise 1/ISP.py
@@ -1,1 +1,23 @@
+from abc import ABC, abstractmethod
 
+class Printer(ABC):
+    @abstractmethod
+    def print_doc(self):
+        pass
+
+class Scanner(ABC):
+    @abstractmethod
+    def scan_doc(self):
+        pass
+
+class MultiFunctionMachine(Printer, Scanner):
+
+    def print_doc(self):
+        print("Printing")
+
+    def scan_doc(self):
+        print("Scanning")
+
+m = MultiFunctionMachine()
+m.print_doc()
+m.scan_doc()
