4605cacbc9ffd3d5de0d7ad05a9284bfbf3192bb
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-06-30 11:22:25 +0530

    Update Single Responsibility Principle.js

--- Deepskilling/Module1/Design Patterns and Principles/Exercise 3/Single Responsibility Principle.js
+++ Deepskilling/Module1/Design Patterns and Principles/Exercise 3/Single Responsibility Principle.js
@@ -1,1 +1,21 @@
+class Report{
 
+    constructor(content){
+        this.content = content;
+    }
+
+}
+
+class ReportPrinter{
+
+    print(report){
+        console.log(report.content);
+    }
+
+}
+
+let report = new Report("Monthly Report");
+
+let printer = new ReportPrinter();
+
+printer.print(report);
