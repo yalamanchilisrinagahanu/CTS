a437bfd49da1427c6505b24471c4f4fe5f9b8c73
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-06-30 11:06:28 +0530

    Update Strategy.py

--- Deepskilling/Module1/Design Patterns and Principles/Exercise 2/Strategy.py
+++ Deepskilling/Module1/Design Patterns and Principles/Exercise 2/Strategy.py
@@ -1,1 +1,17 @@
+class Add:
 
+    def execute(self, a, b):
+        return a + b
+
+class Multiply:
+
+    def execute(self, a, b):
+        return a * b
+
+strategy = Add()
+
+print(strategy.execute(5, 4))
+
+strategy = Multiply()
+
+print(strategy.execute(5, 4))
