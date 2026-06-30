d7c0deb5c2e88545adcd7493a6ad2aa3af53743b
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-06-30 09:47:10 +0530

    Update Singleton.py

--- Deepskilling/Module1/Design Patterns and Principles/Exercise 1/Singleton.py
+++ Deepskilling/Module1/Design Patterns and Principles/Exercise 1/Singleton.py
@@ -1,1 +1,15 @@
+class Singleton:
 
+    _instance = None
+
+    def __new__(cls):
+
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+
+        return cls._instance
+
+a = Singleton()
+b = Singleton()
+
+print(a == b)
