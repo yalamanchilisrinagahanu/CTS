0558caf4042fa4fe3e031e05d5cf6360981d1ad0
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-06-30 09:12:20 +0530

    Update Single Responsibility Principle.py

--- Deepskilling/Module1/Design Patterns and Principles/Exercise 1/Single Responsibility Principle.py
+++ Deepskilling/Module1/Design Patterns and Principles/Exercise 1/Single Responsibility Principle.py
@@ -1,1 +1,26 @@
+# Single Responsibility Principle (SRP)
+class Report:
+    def __init__(self, content):
+        self.content = content
 
+
+class ReportPrinter:
+    def print_report(self, report):
+        print("Report Content:")
+        print(report.content)
+
+
+class ReportSaver:
+    def save_report(self, report):
+        print("Saving report...")
+        print("Report saved successfully.")
+
+
+# Main Program
+report = Report("Monthly Sales Report")
+
+printer = ReportPrinter()
+printer.print_report(report)
+
+saver = ReportSaver()
+saver.save_report(report)
