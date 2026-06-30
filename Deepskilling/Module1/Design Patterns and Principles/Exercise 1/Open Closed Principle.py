bf5bd8b19c91f5e892b07befb0bf40a74697d2ff
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-06-30 09:14:30 +0530

    Update Open Closed Principle.py

--- Deepskilling/Module1/Design Patterns and Principles/Exercise 1/Open Closed Principle.py
+++ Deepskilling/Module1/Design Patterns and Principles/Exercise 1/Open Closed Principle.py
@@ -1,1 +1,26 @@
+from abc import ABC, abstractmethod
 
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, l, w):
+        self.l = l
+        self.w = w
+
+    def area(self):
+        return self.l * self.w
+
+class Circle(Shape):
+    def __init__(self, r):
+        self.r = r
+
+    def area(self):
+        return 3.14 * self.r * self.r
+
+shapes = [Rectangle(10, 5), Circle(7)]
+
+for s in shapes:
+    print(s.area())
