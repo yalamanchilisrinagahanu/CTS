9a7f59456c4bc7766b1f330294e51c6e5743c275
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-06-30 11:07:35 +0530

    Implement Command pattern for Light control

--- Deepskilling/Module1/Design Patterns and Principles/Exercise 2/Command.py
+++ Deepskilling/Module1/Design Patterns and Principles/Exercise 2/Command.py
@@ -1,1 +1,18 @@
+class Light:
 
+    def on(self):
+        print("Light ON")
+
+class Command:
+
+    def __init__(self, light):
+        self.light = light
+
+    def execute(self):
+        self.light.on()
+
+light = Light()
+
+cmd = Command(light)
+
+cmd.execute()
