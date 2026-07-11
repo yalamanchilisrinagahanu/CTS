3fa03369c1ed9dfa593763d49f631df05238bfd9
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-11 15:03:50 +0530

    Update Notification.cs

--- Deepskilling/Module4/NUnit and Moq/Notification.cs
+++ Deepskilling/Module4/NUnit and Moq/Notification.cs
@@ -1,1 +1,14 @@
+public class Notification
+{
+    private readonly IMessageService service;
 
+    public Notification(IMessageService service)
+    {
+        this.service = service;
+    }
+
+    public void Notify()
+    {
+        service.Send();
+    }
+}
