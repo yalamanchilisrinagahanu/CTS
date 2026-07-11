d1577867202b947291c9dd37f19d27efdb0acb4a
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-11 15:11:13 +0530

    Update Calculator.cs

--- Deepskilling/Module4/Hands-On/Calculator.cs
+++ Deepskilling/Module4/Hands-On/Calculator.cs
@@ -1,1 +1,32 @@
+using System;
 
+namespace NUnitMoqHandsOn
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+
+            return a / b;
+        }
+    }
+}
