28e920730dd0961ed0b4e244e9fe55d9c980022b
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-11 15:07:29 +0530

    Update ProductTests.cs

--- Deepskilling/Module4/NUnit and Moq/ProductTests.cs
+++ Deepskilling/Module4/NUnit and Moq/ProductTests.cs
@@ -1,1 +1,19 @@
+using NUnit.Framework;
 
+[TestFixture]
+public class ProductTests
+{
+    [Test]
+    public void AddStock_UpdatesQuantity()
+    {
+        Product product =
+            new Product(10);
+
+        product.AddStock(5);
+
+        Assert.AreEqual(
+            15,
+            product.Quantity
+        );
+    }
+}
