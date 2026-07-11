7f916e38846b345b4eb0b9cdb8097cd002bbd1cd
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-11 15:05:57 +0530

    Add unit test for Notify method in Notification class

--- Deepskilling/Module4/NUnit and Moq/NotificationTests.cs
+++ Deepskilling/Module4/NUnit and Moq/NotificationTests.cs
@@ -1,1 +1,23 @@
+using NUnit.Framework;
+using Moq;
 
+[TestFixture]
+public class NotificationTests
+{
+    [Test]
+    public void Notify_CallsSendMethod()
+    {
+        var mock =
+            new Mock<IMessageService>();
+
+        Notification notification =
+            new Notification(mock.Object);
+
+        notification.Notify();
+
+        mock.Verify(
+            x => x.Send(),
+            Times.Once
+        );
+    }
+}
