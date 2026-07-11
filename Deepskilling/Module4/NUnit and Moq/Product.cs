12da038fff96090411248538715173e189929023
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-11 15:06:43 +0530

    Add Product class with stock management methods

--- Deepskilling/Module4/NUnit and Moq/Product.cs
+++ Deepskilling/Module4/NUnit and Moq/Product.cs
@@ -1,1 +1,14 @@
+public class Product
+{
+    public int Quantity { get; private set; }
 
+    public Product(int quantity)
+    {
+        Quantity = quantity;
+    }
+
+    public void AddStock(int count)
+    {
+        Quantity += count;
+    }
+}
