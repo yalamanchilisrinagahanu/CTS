437454bb78be0d53b7d6724b4896d45a9fd2b9ae
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-11 14:54:15 +0530

    Update CalculatorTests.cs

--- Deepskilling/Module4/NUnit and Moq/CalculatorTests.cs
+++ Deepskilling/Module4/NUnit and Moq/CalculatorTests.cs
@@ -1,1 +1,15 @@
+using NUnit.Framework;
 
+[TestFixture]
+public class CalculatorTests
+{
+    [Test]
+    public void Add_TwoNumbers_ReturnsSum()
+    {
+        Calculator calculator = new Calculator();
+
+        int result = calculator.Add(10, 20);
+
+        Assert.AreEqual(30, result);
+    }
+}
