f3f7d6c5ea9d19b7dd9657a8ce3452b4c50ff61b
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-22 09:41:26 +0530

    Update Program.cs

--- Deepskilling/Module5/Program/Program.cs
+++ Deepskilling/Module5/Program/Program.cs
@@ -1,1 +1,24 @@
+using EntityFrameworkCore8.Data;
+using EntityFrameworkCore8.Models;
 
+AppDbContext db = new AppDbContext();
+
+Department dept = new Department()
+{
+    DepartmentName = "IT"
+};
+
+db.Departments.Add(dept);
+db.SaveChanges();
+
+Student student = new Student()
+{
+    Name = "Rahul",
+    Age = 22,
+    DepartmentId = dept.DepartmentId
+};
+
+db.Students.Add(student);
+db.SaveChanges();
+
+Console.WriteLine("Record Inserted");
