4d60deaf91d91baa866d8de1e131593c63ab92a1
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-22 09:40:15 +0530

    Update AppDbContext.cs

--- Deepskilling/Module5/Data/AppDbContext.cs
+++ Deepskilling/Module5/Data/AppDbContext.cs
@@ -1,1 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using EntityFrameworkCore8.Models;
 
+namespace EntityFrameworkCore8.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=.;Database=StudentDB;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+    }
+}
