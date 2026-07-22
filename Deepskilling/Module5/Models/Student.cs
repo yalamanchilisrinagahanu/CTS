71c53c1bb34517a64a1e34ebbfe195f799c089ae
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-22 09:30:35 +0530

    Update Student.cs

--- Deepskilling/Module5/Models/Student.cs
+++ Deepskilling/Module5/Models/Student.cs
@@ -1,1 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace EntityFrameworkCore8.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public Department Department { get; set; }
+    }
+}
