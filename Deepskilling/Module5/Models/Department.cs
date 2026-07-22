c27587e6a46ce3d94dc437e9cb5d914e15033e4c
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-22 09:31:16 +0530

    Update Department.cs

--- Deepskilling/Module5/Models/Department.cs
+++ Deepskilling/Module5/Models/Department.cs
@@ -1,1 +1,14 @@
+using System.ComponentModel.DataAnnotations;
 
+namespace EntityFrameworkCore8.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+
+        public string DepartmentName { get; set; }
+
+        public List<Student> Students { get; set; }
+    }
+}
