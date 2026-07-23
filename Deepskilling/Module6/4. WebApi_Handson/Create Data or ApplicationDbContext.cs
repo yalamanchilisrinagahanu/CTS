f792e352e7626e14593715e8369e528c074b0534
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-23 21:51:49 +0530

    Update Create Data or ApplicationDbContext.cs

--- Deepskilling/Module6/4. WebApi_Handson/Create Data or ApplicationDbContext.cs
+++ Deepskilling/Module6/4. WebApi_Handson/Create Data or ApplicationDbContext.cs
@@ -1,1 +1,17 @@
+using System.ComponentModel.DataAnnotations;
 
+namespace EmployeeCrudApi.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = "";
+
+        public string Department { get; set; } = "";
+
+        public decimal Salary { get; set; }
+    }
+}
