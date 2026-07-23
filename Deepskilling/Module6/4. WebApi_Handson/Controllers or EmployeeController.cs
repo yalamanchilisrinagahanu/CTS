1cbaa514ca6017a75ab95ff9e56d322447dd5dc7
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-23 21:52:37 +0530

    Update Controllers or EmployeeController.cs

--- Deepskilling/Module6/4. WebApi_Handson/Controllers or EmployeeController.cs
+++ Deepskilling/Module6/4. WebApi_Handson/Controllers or EmployeeController.cs
@@ -1,1 +1,78 @@
+using EmployeeCrudApi.Data;
+using EmployeeCrudApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
+namespace EmployeeCrudApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET
+        [HttpGet]
+        public async Task<IActionResult> GetEmployees()
+        {
+            return Ok(await _context.Employees.ToListAsync());
+        }
+
+        // GET BY ID
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee == null)
+                return NotFound();
+
+            return Ok(employee);
+        }
+
+        // POST
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+
+        // PUT
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEmployee(int id, Employee employee)
+        {
+            if (id != employee.Id)
+                return BadRequest();
+
+            _context.Entry(employee).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+
+        // DELETE
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee == null)
+                return NotFound();
+
+            _context.Employees.Remove(employee);
+
+            await _context.SaveChangesAsync();
+
+            return Ok("Employee Deleted Successfully");
+        }
+    }
+}
