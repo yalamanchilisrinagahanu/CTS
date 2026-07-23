c0441936e90c579735df2234cc78809a89fa8291
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-23 22:15:14 +0530

    Update Services or KafkaConsumer.cs

--- Deepskilling/Module6/6. WebApi_Handson/Services or KafkaConsumer.cs
+++ Deepskilling/Module6/6. WebApi_Handson/Services or KafkaConsumer.cs
@@ -1,1 +1,40 @@
+using Confluent.Kafka;
 
+namespace KafkaChatApp.Services
+{
+    public class KafkaConsumer : BackgroundService
+    {
+        private readonly IConfiguration _configuration;
+
+        public KafkaConsumer(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            return Task.Run(() =>
+            {
+                var config = new ConsumerConfig
+                {
+                    BootstrapServers = _configuration["Kafka:BootstrapServers"],
+                    GroupId = "chat-group",
+                    AutoOffsetReset = AutoOffsetReset.Earliest
+                };
+
+                using var consumer =
+                    new ConsumerBuilder<Ignore, string>(config).Build();
+
+                consumer.Subscribe(_configuration["Kafka:Topic"]);
+
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    var result = consumer.Consume(stoppingToken);
+
+                    Console.WriteLine("Received:");
+                    Console.WriteLine(result.Message.Value);
+                }
+            });
+        }
+    }
+}
