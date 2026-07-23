aa0c132b86ce4966ad7927370b70c7667baaab6f
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-23 21:54:04 +0530

    Update Program.cs

--- Deepskilling/Module6/4. WebApi_Handson/Program.cs
+++ Deepskilling/Module6/4. WebApi_Handson/Program.cs
@@ -1,1 +1,38 @@
+using EmployeeCrudApi.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Employee CRUD API",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
