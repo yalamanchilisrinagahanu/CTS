53e6c7bc1e65a286e5a21a553bb8b2bf4e818f20
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-23 22:13:22 +0530

    Update Models or ChatMessage.cs

--- Deepskilling/Module6/6. WebApi_Handson/Models or ChatMessage.cs
+++ Deepskilling/Module6/6. WebApi_Handson/Models or ChatMessage.cs
@@ -1,1 +1,11 @@
+namespace KafkaChatApp.Models
+{
+    public class ChatMessage
+    {
+        public string User { get; set; } = "";
 
+        public string Message { get; set; } = "";
+
+        public DateTime Time { get; set; } = DateTime.Now;
+    }
+}
