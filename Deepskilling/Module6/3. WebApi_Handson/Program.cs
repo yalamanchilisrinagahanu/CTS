60790cfcae4bb1cec6d0d3806295cfa88789d691
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-23 21:46:38 +0530

    Update Program.cs

--- Deepskilling/Module6/3. WebApi_Handson/Program.cs
+++ Deepskilling/Module6/3. WebApi_Handson/Program.cs
@@ -1,1 +1,32 @@
+using Microsoft.OpenApi.Models;
 
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Employee Routing API",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
