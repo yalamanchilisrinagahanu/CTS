1ab8f4e8f68a660f76ea98c19d592b0d5d260c15
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-23 22:07:30 +0530

    Add LoginModel class for authentication

--- Deepskilling/Module6/5. WebApi_Handson/Models or LoginModel.cs
+++ Deepskilling/Module6/5. WebApi_Handson/Models or LoginModel.cs
@@ -1,1 +1,9 @@
+namespace JwtAuthApi.Models
+{
+    public class LoginModel
+    {
+        public string Username { get; set; } = "";
 
+        public string Password { get; set; } = "";
+    }
+}
