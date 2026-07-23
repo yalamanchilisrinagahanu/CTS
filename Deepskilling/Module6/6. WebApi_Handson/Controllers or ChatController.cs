7910e05df211c4076e1f9008cbe237dbee5c701a
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-23 22:16:06 +0530

    Update Controllers or ChatController.cs

--- Deepskilling/Module6/6. WebApi_Handson/Controllers or ChatController.cs
+++ Deepskilling/Module6/6. WebApi_Handson/Controllers or ChatController.cs
@@ -1,1 +1,32 @@
+using KafkaChatApp.Models;
+using KafkaChatApp.Services;
+using Microsoft.AspNetCore.Mvc;
 
+namespace KafkaChatApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ChatController : ControllerBase
+    {
+        private readonly KafkaProducer _producer;
+
+        public ChatController(KafkaProducer producer)
+        {
+            _producer = producer;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Send(ChatMessage message)
+        {
+            await _producer.SendMessage(message);
+
+            return Ok("Message Sent Successfully");
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Kafka Chat API Running");
+        }
+    }
+}
