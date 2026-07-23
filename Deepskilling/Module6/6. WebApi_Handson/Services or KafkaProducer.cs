874c31cc9e179f11c555fa859620ea8fdc6ab54c
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-23 22:14:08 +0530

    Update Services or KafkaProducer.cs

--- Deepskilling/Module6/6. WebApi_Handson/Services or KafkaProducer.cs
+++ Deepskilling/Module6/6. WebApi_Handson/Services or KafkaProducer.cs
@@ -1,1 +1,36 @@
+using Confluent.Kafka;
+using System.Text.Json;
+using KafkaChatApp.Models;
 
+namespace KafkaChatApp.Services
+{
+    public class KafkaProducer
+    {
+        private readonly IConfiguration _configuration;
+
+        public KafkaProducer(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendMessage(ChatMessage message)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = _configuration["Kafka:BootstrapServers"]
+            };
+
+            using var producer =
+                new ProducerBuilder<Null, string>(config).Build();
+
+            var json = JsonSerializer.Serialize(message);
+
+            await producer.ProduceAsync(
+                _configuration["Kafka:Topic"],
+                new Message<Null, string>
+                {
+                    Value = json
+                });
+        }
+    }
+}
