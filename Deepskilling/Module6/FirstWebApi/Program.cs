8dc4163e039f533cc4e4a30da6bb5beadbff1c89
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-23 21:05:49 +0530

    Update Program.cs

--- Deepskilling/Module6/FirstWebApi/Program.cs
+++ Deepskilling/Module6/FirstWebApi/Program.cs
@@ -1,1 +1,34 @@
+using Microsoft.OpenApi.Models;
 
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "First Web API",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+
+// Configure middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
