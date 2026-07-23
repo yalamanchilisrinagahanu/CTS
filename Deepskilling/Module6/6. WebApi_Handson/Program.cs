dcd3f7c3eec614779f61cd76eee783e2b92f1b72
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-23 22:16:41 +0530

    Set up Web API with Swagger and Kafka services

--- Deepskilling/Module6/6. WebApi_Handson/Program.cs
+++ Deepskilling/Module6/6. WebApi_Handson/Program.cs
@@ -1,1 +1,36 @@
+using KafkaChatApp.Services;
+using Microsoft.OpenApi.Models;
 
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddSingleton<KafkaProducer>();
+
+builder.Services.AddHostedService<KafkaConsumer>();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+        new OpenApiInfo
+        {
+            Title = "Kafka Chat API",
+            Version = "v1"
+        });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
