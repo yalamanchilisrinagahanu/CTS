1628e90d3718895fc1ddb0bf9acea1057c5de9f5
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-23 22:27:49 +0530

    Update SecureController.cs

--- Deepskilling/Module 7 - Microservices Architecture using ASP.NET Core Web API/1. Microservices-jwt/Controllers/SecureController.cs
+++ Deepskilling/Module 7 - Microservices Architecture using ASP.NET Core Web API/1. Microservices-jwt/Controllers/SecureController.cs
@@ -1,1 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
+namespace JwtMicroservice.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SecureController : ControllerBase
+    {
+        // Question 2: Secure an endpoint using [Authorize]
+        [HttpGet("data")]
+        [Authorize]
+        public IActionResult GetSecureData()
+        {
+            return Ok("This is protected data.");
+        }
+    }
+}
