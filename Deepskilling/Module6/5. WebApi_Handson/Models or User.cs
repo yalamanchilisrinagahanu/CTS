295dafc9df683790ce6e2f1a48a72106221b3e52
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-23 22:06:34 +0530

    Update Models or User.cs

--- Deepskilling/Module6/5. WebApi_Handson/Models or User.cs
+++ Deepskilling/Module6/5. WebApi_Handson/Models or User.cs
@@ -1,1 +1,13 @@
+namespace JwtAuthApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
 
+        public string Username { get; set; } = "";
+
+        public string Password { get; set; } = "";
+
+        public string Role { get; set; } = "";
+    }
+}
