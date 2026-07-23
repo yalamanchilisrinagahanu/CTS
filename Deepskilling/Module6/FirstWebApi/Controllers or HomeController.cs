ff938051f7d536217eea2902e8f576455958ca77
yalamanchilisrinagahanu <yalamanchilisrinagahanu@gmail.com>
2026-07-23 21:07:08 +0530

    Add HomeController with basic API endpoints

--- Deepskilling/Module6/FirstWebApi/Controllers or HomeController.cs
+++ Deepskilling/Module6/FirstWebApi/Controllers or HomeController.cs
@@ -1,1 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
 
+namespace FirstWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Welcome to ASP.NET Core 8 Web API");
+        }
+
+        [HttpGet("hello")]
+        public IActionResult Hello()
+        {
+            return Ok("Hello Cognizant Learner!");
+        }
+
+        [HttpGet("about")]
+        public IActionResult About()
+        {
+            return Ok(new
+            {
+                Name = "First Web API",
+                Version = ".NET 8",
+                Author = "Student"
+            });
+        }
+    }
+}
